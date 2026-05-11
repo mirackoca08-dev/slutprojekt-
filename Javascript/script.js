ab4a5f35de8a6747289cca38835881b0d0c89f88
mirackoca08-dev <mirac.koca08@gmail.com>
2026-05-11 12:49:22 +0200

    ronaldo

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,31 +1,3 @@
-function rattaQuiz() {
-
-    let q1 = document.getElementById("q1").value.toLowerCase().trim();
-    let q2 = document.getElementById("q2").value.toLowerCase().trim();
-    let q3 = document.getElementById("q3").value.toLowerCase().trim();
-
-    let poang = 0;
-
-    if (q1.includes("kraft")) {
-        poang++;
-    }
-
-    if (q2.includes("första") || q2.includes("tröghet")) {
-        poang++;
-    }
-
-    if (
-        q3.includes("lika stor") ||
-        q3.includes("motsatt") ||
-        q3.includes("reaktion")
-    ) {
-        poang++;
-    }
-
-    document.getElementById("resultat").textContent =
-        "Du fick " + poang + " av 3 rätt!";
-}  
-
 function sanitize(text) {
     return text
         .toLowerCase()
--- README.md
+++ README.md
@@ -35,3 +35,5 @@ Exempel från Newtons vagga används för att visa rörelse och energiöverföri
 - HTML
 - CSS
 - JavaScript
+
+![Min manuell test](Skärmbild 2026-05-11 120804.png)
