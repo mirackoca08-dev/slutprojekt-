961ee52bcb6a71f8819523a0bc5ff29350c5a52e
mirackoca08-dev <Mirac.koca08@gmail.com>
2026-05-21 13:51:08 +0200

    emannuel stinker idag

--- script.js
+++ script.js
@@ -1,33 +1,5 @@
 // Funktion som gör användarens text säkrare
 // Skyddar mot enkla XSS-attacker genom att ta bort < och > 
-function rattaQuiz() {
-
-    let q1 = document.getElementById("q1").value.toLowerCase().trim(); 
-    let q2 = document.getElementById("q2").value.toLowerCase().trim(); 
-    let q3 = document.getElementById("q3").value.toLowerCase().trim();
-
-    let poang = 0;
-
-    if (q1.includes("kraft")) {
-        poang++;
-    }
-
-    if (q2.includes("första") || q2.includes("tröghet")) {
-        poang++;
-    }
-
-    if (
-        q3.includes("lika stor") ||
-        q3.includes("motsatt") ||
-        q3.includes("reaktion")
-    ) {
-        poang++;
-    }
-
-    document.getElementById("resultat").textContent =
-        "Du fick " + poang + " av 3 rätt!";
-}  
-
 function sanitize(text) {
     return text
         .toLowerCase() // Gör all text till små bokstäver 
